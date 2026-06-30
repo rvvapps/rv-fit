b8172d3e3afa5759947b01471f9022a732b67791
rvvapps <rodolfo.vassallo@gmail.com>
2026-06-30 10:05:48 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* sw.js — RV FIT (GitHub Pages: /rv-fit/) */
 
-const VERSION = "rvfit-v3-2026-02-06";
+const VERSION = "rvfit-v4-2026-06-30";
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const BASE = "/rv-fit/";
